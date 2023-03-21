73db8438199215ddbe4f0b8d26332882d1de8027
byMagg <byMag@hotmail.es>
2023-03-21 17:54:32 +0100

    add objectId

--- activity-4/app_server/models/locations.js
+++ activity-4/app_server/models/locations.js
@@ -1,33 +1,34 @@
 const mongoose = require('mongoose');
 
 const openingTimeSchema = new mongoose.Schema({
-    days: {type: String, required: true},
+    days: { type: String, required: true },
     opening: String,
     closing: String,
-    closed: {type: Boolean, required: true}
+    closed: { type: Boolean, required: true }
 });
 
 const reviewSchema = new mongoose.Schema({
+    _id: { type: mongoose.Schema.ObjectId },
     author: String,
-    rating: {type: Number, required: true, min: 0, max: 5},
+    rating: { type: Number, required: true, min: 0, max: 5 },
     reviewText: String,
-    createdOn: {type: Date, "default": Date.now }
+    createdOn: { type: Date, "default": Date.now }
 });
 
 const locationSchema = new mongoose.Schema({
-    name: {type: String, required: true},
+    name: { type: String, required: true },
     address: String,
-    rating: {type: Number, "default": 0, min: 0, max: 5},
+    rating: { type: Number, "default": 0, min: 0, max: 5 },
     facilities: [String],
     // Always store coordinates longitude, latitude order.
     coords: {
-        type: {type: String},
+        type: { type: String },
         coordinates: [Number]
     },
     openingTimes: [openingTimeSchema],
     reviews: [reviewSchema]
 });
 
-locationSchema.index({coords: '2dsphere'})
+locationSchema.index({ coords: '2dsphere' })
 
 mongoose.model('Location', locationSchema);
