3b988e6987511b1a2d98728195a449bd3209893c
byMagg <byMag@hotmail.es>
2023-04-11 20:13:08 +0200

    remove nos used

--- activity-5/app.js
+++ activity-5/app.js
@@ -1,24 +1,16 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const favicon = require('serve-favicon');
-require('./app_api/models/db');
+require('./api/models/db');
 
-// const indexRouter = require('./app_server/routes/index');
-const apiRouter = require('./app_api/routes/index');
+const apiRouter = require('./api/routes/index');
 
 const app = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'app_server', 'views'));
-// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRouter);
